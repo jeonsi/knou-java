7bc81b75a2b526a92e9d3cb01fbe2cb6b4bb5f15
Seung-il Jeon <si.j@navercorp.com>
2023-02-28 10:18:39 +0900

    first commit

---
+++ ch01/HelloApplication.java
@@ -0,0 +1,9 @@
+/**
+ * The HelloApplication class implements an application that
+ * simply prints "Hello, Java Application!" th standard output.
+ */
+public class HelloApplication {
+    public static void main(String[] args) { // main method
+        System.out.println("Hello, Java Application!");
+    }
+}
